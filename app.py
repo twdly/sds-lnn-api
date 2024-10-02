853926ed1c8fdb05782079675351c87adbb302d7
twdly <taiwoodley@gmail.com>
2024-10-02 14:31:50 +1000

    Return json for get method

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Response
 from flask import request
 
 import lnn
@@ -8,19 +9,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def get_json():
-    state = lnn.get_state()
-    prediction = lnn.get_prediction()
-    return f"Prediction: {prediction}, vorticity: {state[0]}, temperature: {state[1]}"
+    return lnn.get_state()
 
 
 # This method will need to be secured somehow in the future to prevent unauthorised people from sending in weather data
-@app.route("/set", methods=['POST'])
+@app.route("/set", methods=['GET'])
 def set_state():
     args = request.args
     temperature = args.get("temp")
     vorticity = args.get("vort")
     lnn.update_state(vorticity, temperature)
-    return 'success'
+    return Response(status=200)
 
 
 if __name__ == "__main__":
--- lnn.py
+++ lnn.py
@@ -1,18 +1,25 @@
 from random import random
 
 
-def get_prediction():
-    return random() * 100
-
-
+# This function simulates receiving new input data and having the LNN making a prediction with that new data
+# In the future, get_prediction() will instead create a new tensor with the data and pass it into the LNN
 def update_state(vorticity, temperature):
+
+    def get_prediction():
+        return random() * 100
+
     file = open("state.txt", "a")
-    file.write(f"\n{vorticity},{temperature}")
+    file.write(f"{round(get_prediction(), 2)},{vorticity},{temperature}\n")
     file.close()
 
 
 def get_state():
     file = open("state.txt", "r")
     lines = file.readlines()
-    current_state = lines[len(lines) - 1]
-    return current_state.split(",")
+    current_state = lines[len(lines) - 1].removesuffix("\n").split(",")
+    result = {
+        "prediction": current_state[0],
+        "vorticity": current_state[1],
+        "temperature": current_state[2]
+    }
+    return result
